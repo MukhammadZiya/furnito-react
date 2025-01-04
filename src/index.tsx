ac290c98a70f9e1a578d17c1b650d6bdafc91b3a
MukhammadZiya <mukhammadziyaa@gmail.com>
2025-01-05 08:22:17 +0900

    feat: MUi integration

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,9 @@ import { Provider } from "react-redux";
 import { store } from "./app/store";
 import App from "./app/App";
 import reportWebVitals from "./reportWebVitals";
+import CssBaseline from "@mui/material/CssBaseline";
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "./theme"
 import "./css/index.css";
 
 ReactDOM.render(
